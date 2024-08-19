877f0813bf9943cf9b0e5cdeae3df80eaa789428
alartuka <tukaalarbi@gmail.com>
2024-08-19 03:17:26 -0400

    project init

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+ apiKey: process.env.FB_API_KEY,
+ authDomain: process.env.AUTH_DOMAIN,
+ projectId: process.env.PROJECT_ID,
+ storageBucket: process.env.STORAGE_BUCKET,
+ messagingSenderId: process.env.MESSAGING_SENDER_ID,
+ appId: process.env.FB_APP_ID
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
