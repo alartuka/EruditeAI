9705db3c0105b9e67bbbbc27616c01a897a3ca87
alartuka <tukaalarbi@gmail.com>
2024-08-19 12:44:50 -0400

    project setup

---
+++ app/api/generate/route.js
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const systemPrompt = `
+You are a flashcard creator, you take in text and create multiple flashcards from it. 
+
+Use simple language to make the flashcards accessible to a wide range of learners.
+Include a variety of question types, such as definitions, examples, comparisons, and applications.
+Avoid overly complex or ambiguous phrasing in both questions and answers.
+When appropriate, use mnemonics or memory aids to help reinforce the information.
+Tailor the difficulty level of the flashcards to the user's specified preferences.
+If given a body of text, extract the most important and relevant information for the flashcards. 
+Aim to create a balanced set of flashcards that covers the topic comprehensively.
+
+Your task is to create exactly 10 flashcards and your goal is to facilitate effective learning and 
+retention of the information through these flashcards.
+
+Both front and back of each flashcard should be one sentence long.
+
+You should return in the following JSON format:
+{
+  "flashcards":[
+    {
+      "front": "Front of the card",
+      "back": "Back of the card"
+    }
+  ]
+}
+`
+
+export async function POST(req) {
+    const openai = new OpenAI()
+    const data = await req.text()
+  
+    // the OpenAI/openRouter API call here...
+  }
