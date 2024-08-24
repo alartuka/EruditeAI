e46d7f9f0d13e8a908fdfc9f44fbb7a9ac00e58d
alartuka <tukaalarbi@gmail.com>
2024-08-24 13:47:30 -0400

    flashcards/page updated. Delete set functionality and ui added

--- app/flashcards/page.js
+++ app/flashcards/page.js
@@ -2,9 +2,11 @@
 import { useState, useEffect } from 'react'
 import { useUser } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
-import { Container, Grid, Card, CardActionArea, CardContent, Typography, CircularProgress } from '@mui/material'
-import { doc, getDoc, setDoc, collection } from 'firebase/firestore'
+import DeleteIcon from '@mui/icons-material/Delete'
+import { Container, Grid, Card, CardActionArea, CardContent, Typography, CircularProgress, IconButton, Tooltip, CardHeader, Fab, Stack, CardActions, Box } from '@mui/material'
+import { doc, getDoc, setDoc, collection, updateDoc } from 'firebase/firestore'
 import { db } from '../../firebase'
+import { Delete } from '@mui/icons-material'
 
 export default function Flashcards() {
     const { isLoaded, isSignedIn, user } = useUser()
@@ -33,6 +35,24 @@ export default function Flashcards() {
       getFlashcards() 
     }, [user])
 
+
+    // ==== DELETE SET ====
+    const handleDeleteSet = async (setID) => {
+      if (!user) return
+
+      const docRef = doc(collection(db, 'users'), user.id)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+          const currentFlashcards = docSnap.data().flashcards || []
+          const updatedFlashcards = currentFlashcards.filter((flashcard) => flashcard.name !== setID)
+
+          await updateDoc(docRef, { flashcards: updatedFlashcards })
+          setFlashcards(updatedFlashcards)
+      }
+    }
+
+    // ==== LOADING PAGE ====
     if (!isLoaded || !isSignedIn) {
       return (
         <Container maxWidth="sm" sx={{textAlign: 'center', mt: 4}}>
@@ -50,13 +70,24 @@ export default function Flashcards() {
           {flashcards.map((flashcard, index) => (
             <Grid item xs={12} sm={6} md={4} key={index}>
               <Card>
-                <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
-                  <CardContent>
-                    <Typography variant="h5" component="div">
-                      {flashcard.name}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
+                <Stack direction='row' spacing={3}>
+                  <Box sx={{ p: 2 }}>
+                    <Tooltip title="Delete Set" placement="top" arrow>
+                      <Fab color="error" aria-label="delete set" 
+                      onClick={() => {handleDeleteSet(flashcard.name)}}>
+                        <Delete />
+                      </Fab>
+                    </Tooltip>
+                  </Box>
+
+                  <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        {flashcard.name}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Stack>
               </Card>
             </Grid>
           ))}
